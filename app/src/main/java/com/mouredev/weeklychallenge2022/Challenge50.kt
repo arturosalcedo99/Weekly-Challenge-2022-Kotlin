a99b031d3ad28f310984705716cdb07230227239
ArturoRudo <104420655+arturosalcedo99@users.noreply.github.com>
2022-12-13 17:01:46 +0100

    Challenge50 add decryption

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge50.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge50.kt
@@ -11,21 +11,67 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
+private val vowelPositions: HashMap<Char, ArrayList<Int>> = hashMapOf()
+
 fun main() {
-    println("－－－－－－－－－－－－－－－－－－")
-    println("| Karaca encryption examples |")
-    println("| Banana --> ${"Banana".encryptedByKaraca()}\t\t |")
-    println("| Karaca --> ${"Karaca".encryptedByKaraca()}\t\t |")
-    println("| Burak --> ${"Burak".encryptedByKaraca()}\t\t |")
-    println("| Alpaca --> ${"Alpaca".encryptedByKaraca()}\t\t |")
-    println("－－－－－－－－－－－－－－－－－－")
+    println("－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－")
+    println("| Karaca encryption examples | Karaca decryption examples |")
+
+    val banana = "Banana".karacaEncryption()
+    val bananaDecrypted = banana.karacaEncryption(true)
+    println("| Banana --> $banana\t\t | $banana --> $bananaDecrypted\t\t  |")
+
+    val karaca = "Karaca".karacaEncryption()
+    val karacaDecrypted = karaca.karacaEncryption(true)
+    println("| Karaca --> $karaca\t\t | $karaca --> $karacaDecrypted\t\t  |")
+
+    val burak = "Burak".karacaEncryption()
+    val burakDecrypted = burak.karacaEncryption(true)
+    println("| Burak --> $burak\t\t | $burak --> $burakDecrypted\t\t  |")
+
+    val alpaca = "Alpaca".karacaEncryption()
+    val alpacaDecrypted = alpaca.karacaEncryption(true)
+    println("| Alpaca --> $alpaca\t\t | $alpaca --> $alpacaDecrypted\t\t  |")
+
+    val pepito = "Pepito".karacaEncryption()
+    val pepitoDecrypted = pepito.karacaEncryption(true)
+        println("| Pepito --> $pepito\t\t | $pepito --> $pepitoDecrypted\t\t  |")
+    println("－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－")
 }
 
-private fun String.encryptedByKaraca(): String {
-    return this.reversed().lowercase()
-        .replace('a', '0')
-        .replace('e', '1')
-        .replace('i', '2')
-        .replace('o', '2')
-        .replace('u', '3') + "aca"
+private fun String.karacaEncryption(decrypt: Boolean = false): String {
+
+    val auxCharArray =
+        if (!decrypt)
+            this.reversed().lowercase().toCharArray()
+        else this.lowercase().removeSuffix("aca").toCharArray()
+
+    auxCharArray.forEachIndexed { index: Int, c: Char ->
+        if (!decrypt) {
+            if (vowelPositions.containsKey(c))
+                vowelPositions[c]?.add(index)
+            else vowelPositions[c] = arrayListOf(index)
+        }
+        when (c) {
+            'a' -> auxCharArray[index] = '0'
+            'e' -> auxCharArray[index] = '1'
+            'i', 'o' -> {
+                auxCharArray[index] = '2'
+            }
+            'u' -> auxCharArray[index] = '3'
+            '0' -> auxCharArray[index] = 'a'
+            '1' -> auxCharArray[index] = 'e'
+            '2' -> {
+                auxCharArray[index] =
+                    if (vowelPositions['i']?.any { it == index } == true) 'i' else 'o'
+            }
+            '3' -> auxCharArray[index] = 'u'
+        }
+    }
+
+    return if (!decrypt)
+        auxCharArray.joinToString("") + "aca"
+        else auxCharArray.joinToString("")
+                .reversed()
+                .replaceFirstChar { it.uppercase() }
 }
